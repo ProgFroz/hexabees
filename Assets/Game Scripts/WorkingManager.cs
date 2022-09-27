0e1fbfff423db2224fc1852271c87d5e4d6a3feb
ProgFroz <justin.nicola@gmx.net>
2022-09-27 18:39:01 +0200

    Fixed Build Bug

--- Assets/Game Scripts/WorkingManager.cs
+++ Assets/Game Scripts/WorkingManager.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Linq;
-using Unity.Jobs.LowLevel.Unsafe;
-using UnityEditorInternal;
 using Random = UnityEngine.Random;
 
 public class WorkingManager : MonoBehaviour {
